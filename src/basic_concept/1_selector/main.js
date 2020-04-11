0037ed4c885b8503dda38d41a1d731b794886b19
mc <mc@IdeMacBook-Pro.local>
2020-04-11 22:40:49 +0800

    feat:add more code of demos

--- src/basic_concept/1_selector/main.js
+++ src/basic_concept/1_selector/main.js
@@ -4,16 +4,43 @@ function change_h1() {
     .text('Hello D3.js~~~~')
     .style('font-size', '60px')
     .style('color', 'red');
-
 }
 
 function insert_div_rect() {
-  console.log('insert_div_rect')
   d3.select('.rect-container')
     .html('<div class="rect"></div>')
     .select('.rect')
     .style('width', '300px')
     .style('height', '500px')
     .style('background-color', 'green');
+}
+
+function append_child() {
+  d3.select('.to-be-append')
+    .attr('class', 'append-div')
+    .text('Grandpa')
+    .style('font-size', '40px')
+    .style('width', '300px')
+    .style('height', '500px')
+    .style('background-color', 'yellow')
+    .append('div')
+    .attr('class', 'append-div')
+    .text('Father')
+    .style('font-size', '30px')
+    .style('color', 'white')
+    .style('width', '200px')
+    .style('height', '300px')
+    .style('background-color', 'red')
+    .append('p')
+    .attr('class', 'append-div')
+    .text('Son')
+    .style('font-size', '15px')
+    .style('color', 'white')
+    .style('width', '100px')
+    .style('height', '200px')
+    .style('background-color', 'blue');
+  
+    d3.selectAll('.append-div')
+      .style('border', 'solid 1px black');
 
 }
