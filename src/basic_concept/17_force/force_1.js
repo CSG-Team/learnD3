d350260c2bd4c3de5f13a85b7c3293d5387361cc
yxnne <506524339@qq.com>
2020-06-18 02:24:30 +0800

    feat: 增加注释

--- src/basic_concept/17_force/force_1.js
+++ src/basic_concept/17_force/force_1.js
@@ -1,12 +1,12 @@
 const width = 1000;
-const height = 1000;
-const r = 4;
+const height = 600;
+const r = 3;
 const nodes = [];
 
 const force = d3.forceSimulation()
-  .velocityDecay(0.8)
-  .alphaDecay(0)
-  .force('collision', d3.forceCollide(r + 0.5).strength(1));
+  .velocityDecay(0.8) // 速度衰减 速度越下，跑的越快 设成0就很有意思
+  .alphaDecay(0) // alpha 是 0 表示模拟永不停歇 正常迭代是从1 -> 0 的过程
+  .force('collision', d3.forceCollide(r + 0.5).strength(1)); // collision是碰撞力
 
 const svg = d3.select('body')
   .append('svg')
@@ -48,4 +48,46 @@ svg.on('mousemove', function() {
     force.nodes(nodes);
 
 });
+
+// 无力状态
+function noForce(){
+  force.force('charge', null);
+  force.force('x', null);
+  force.force('y', null);
+  force.restart();
+}
+
+// 排斥状态
+function diss() {
+  force.force('charge', d3.forceManyBody().strength(-10)); // forceManyBody是节点相互作用力 大于0 吸引力
+  force.force('x', null);     // 在某店处增加引力
+  force.force('y', null);
+  force.restart();
+}
+
+// 吸引力
+function like() {
+  force.force('charge', d3.forceManyBody().strength(1));
+  force.force('x', null);
+  force.force('y', null);
+  force.restart();
+}
+
+// 吸引力 , 设置固定引力
+function likeToPosition() {
+  force.force('charge', d3.forceManyBody().strength(1));
+  force.force('x', d3.forceX(width / 2));
+  force.force('y', d3.forceY(height / 2));
+  // force.force('y', null);  // 如果这样就是某个轴方向的吸引力
+
+  force.restart();
+}
+
+// 平衡：定点引力相互斥力
+function balance() {
+  force.force('charge', d3.forceManyBody().strength(-20));
+  force.force('x', d3.forceX(width / 2));
+  force.force('y', d3.forceY(height / 2));
+  force.restart();
+}
 
